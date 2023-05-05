13761483ae938855377694d3658036a14016ed05
vikasgautam1234 <vikasmtr1234@gmail.com>
2023-05-06 02:11:00 +0530

    LAYOUT DONE

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Doctor-Appointment-system
 
 
+<!-- LAYOUT SETUP -->
+1- created file inside COMPONENT FOLDER (Layout.js)
+2- then creted some classes like main, content, header etc and then wrape home page by layout
+3- create a new file inside STYLES folder name LayoutStyles.css then did some styling in the CSS file.
+4- then we have to create a navigation menu in the homepage
+5 - copy the link from FONT-AWSOME and paste it at the index.html and then create the side bar menu in tha DATA FOLDER with help of font awsome and then style the sidebar inside layoutstyle.css
+
+
+<!-- GETTING USER DETAILS -->
---
+++ client/src/Data/data.js
@@ -0,0 +1,27 @@
+export const SidebarMenu = [
+    {
+        name: "Home",
+        path: '/',                
+        icon : "fa-solid fa-house"
+    },
+    {
+        name: "Appointments",
+        path: "/appointments",      //when user click on the appointment then it will redirect it
+        icon: "fa-solid fa-list"
+    },
+    {
+        name: "Apply Doctor",
+        path: "/apply-doctor",       //when user click on the APPLY-DOCTO then it will redirect it
+        icon: "fa-solid fa-user-doctor"
+    },
+    {
+        name: "Profile",
+        path: "/profile",              //when user click on the PROFILE then it will redirect it
+        icon: "fa-solid fa-user"
+    },
+    {
+        name: "Logout",
+        path: "/logout",               //when user click on the LOGOUT then it will redirect it
+        icon: "fa-solid fa-right-from-bracket"
+    }
+]
